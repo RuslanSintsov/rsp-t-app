bfd126ded70e02c417e7ca656a7008722cdfda3a
RuslanSintsov <maeestro@list.ru>
2025-03-16 04:00:21 +0300

    Add Logo component and fix build

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo: React.FC = () => {
+  return (
+    <div className="logo">
+      <span className="logo-text">RSP-T</span>
+    </div>
+  );
+};
+
+export default Logo; 
